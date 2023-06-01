0172d5646bc21d159af2d918f737183fa66a3dad
prasadsawant7 <prasadsawant7.git@gmail.com>
2023-06-02 02:59:25 +0530

    Added Few More Routes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!");
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home Page");
+  } else if (req.url === "/api/employee") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    const employee = {
+      emp_id: 001,
+      name: "Prasad",
+      salary: 100000,
+      department: "Development",
+      branch: "Mumbai",
+    };
+    res.end(JSON.stringify(employee));
+  }
 });
 
 const PORT = process.env.PORT || 3000;
